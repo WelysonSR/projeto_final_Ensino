3537e693eb2ecf932d4c63dfa75c7d2195de68dd
Welyson Rosa <welysonsr@gmail.com>
2023-12-01 11:27:01 -0300

    estilizando page login

--- front-end/src/app/login/page.js
+++ front-end/src/app/login/page.js
@@ -7,37 +7,60 @@ import { setCookie } from 'cookies-next'
 
 
 export default function Login() {
-    const [user, setName] = useState("")
-    const [password, setPassword] = useState("")
-    const {push} = useRouter()
+  const [user, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const { push } = useRouter()
 
-    const login = async (e)=>{
-        e.preventDefault()
-        if (user.length<5) {
-            alert("Nome deve ter no mínimo 5 caracteres") 
-            return 
-        }
-        if (password.length<6){
-            alert("Digite uma senha com no minimo 6 caracteres")
-            return
-        }
-        const result = await loginAxios({user, password})
-
-        if (result.user === user && result.password === createHash('md5').update(password).digest('hex')){
-            setCookie('user', JSON.stringify(result))
-            push("/home")
-        }else {
-            alert("Usuário ou senha incorreto")
-        }
+  const login = async (e) => {
+    e.preventDefault()
+    if (user.length < 5) {
+      alert("Nome deve ter no mínimo 5 caracteres")
+      return
     }
+    if (password.length < 6) {
+      alert("Digite uma senha com no minimo 6 caracteres")
+      return
+    }
+    const result = await loginAxios({ user, password })
 
-    return (
-        <form id="form">
-            <input onChange={({target})=>setName(target.value)} value={user} type="text" name="nome" placeholder="nome de usuario"/>
-            <span class="span-required">Nome deve ter no mínimo 5 caracteres</span>
-            <input onChange={({target})=>setPassword(target.value)} value={password} type="password" name="senha" placeholder="sua senha"/>
-            <span class="span-required">Digite uma senha com no minimo 6 caracteres</span>
-            <input onClick={login} type="submit" value="entrar"/>
-        </form>
-    )
+    if (result.user === user && result.password === createHash('md5').update(password).digest('hex')) {
+      setCookie('user', JSON.stringify(result))
+      push("/home")
+    } else {
+      alert("Usuário ou senha incorreto")
+    }
+  }
+
+  return (
+    <form className="login-form">
+      <input
+        type="text"
+        name="nome"
+        value={user}
+        className="login-input"
+        placeholder="nome de usuario"
+        onChange={({ target }) => setName(target.value)}
+      />
+      <span className="login-span">
+        Nome deve ter no mínimo 5 caracteres
+      </span>
+      <input
+        type="password"
+        name="senha"
+        value={password}
+        className="login-input"
+        placeholder="sua senha"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+      <span className="login-span">
+        Digite uma senha com no minimo 6 caracteres
+      </span>
+      <input
+        type="submit"
+        value="Entrar"
+        className="login-input-submit"
+        onClick={login}
+      />
+    </form>
+  )
 }
