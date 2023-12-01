134d3d053b3b7ad039e24388f4028ee0695da224
Welyson Rosa <welysonsr@gmail.com>
2023-12-01 11:00:20 -0300

    organizando o css da NavBar

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,27 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function NavBar(){
-    return(
-        <nav className="navbar">
-            <div className="navbar-container">
-                <div className="logo">
-                    <Link href="/">
-                        <Image src="/img/logo.png" width={50} height={50} alt="Logo" />
-                    </Link>
-                </div>
-                <div className="menu">
-                    <ul>                  
-                        <li><Link href="/crud_jogos">Cadastrar Jogos</Link></li>
-                        <li><Link href="/crud_plataforma">Cadastrar Plataforma</Link></li>
-                        <li><Link href="/noticias">Noticias</Link></li>
-                        <li><Link href="/comunidade">Comunidade</Link></li>
-                        <li><Link href="/sobre">Sobre Nós</Link></li>
-                        <li><Link href="/login">Login</Link></li>
-                        <li><Link href="/cadastro">Criar Conta</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <div className="nav-div nav-logo">
+        <Link className="nav-logo-link" href="/">
+          <Image src="/img/logo.png" width={40} height={40} alt="Logo" />
+        </Link>
+      </div>
+      <div className="nav-div">
+        <ul className="nav-ul">
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/crud_jogos">Cadastrar Jogos</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/crud_plataforma">Cadastrar Plataforma</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/noticias">Noticias</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/comunidade">Comunidade</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/sobre">Sobre Nós</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/login">Login</Link></li>
+          <li className="nav-li nav-li-link:hover"><Link className="nav-li-link" href="/cadastro">Criar Conta</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
